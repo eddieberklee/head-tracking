6b8afcb19183b44fb232aacd4454ad77b18d3e9e
Eddie Lee <eddieberklee@gmail.com>
2015-10-23 00:52:17 -0700

    working with arduino file: wifi_and_button_for_request_working but http get requests are a little slow - going to try out uDP and see if that's quicker

--- flask-server.py
+++ flask-server.py
@@ -1,15 +1,35 @@
 
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
+
+global whichButton
+whichButton = "default_button"
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  return render_template('home.html')
+  # return "Hello World!"
+
+@app.route("/which_button")
+def which_button():
+  return "" + whichButton
+
+@app.route("/button1")
+def button1():
+  global whichButton
+  whichButton = "Button 1"
+  return whichButton
+
+@app.route("/button2")
+def button2():
+  global whichButton
+  whichButton = "Button 2"
+  return whichButton
 
 if __name__ == "__main__":
-  app.run()
-
-
+  app.run("10.0.42.201", debug = True, threaded = True)
+  whichButton = "hi"
 
 
 
