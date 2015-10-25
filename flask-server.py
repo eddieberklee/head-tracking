e51f9e6b82a9c7f8b1d191013eaf047e05a390fa
Eddie Lee <eddieberklee@gmail.com>
2015-10-25 16:53:28 -0700

    final code, also has a blinking live icon

--- flask-server.py
+++ flask-server.py
@@ -8,7 +8,7 @@ whichButton = "default_button"
 
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('index.html')
   # return "Hello World!"
 
 @app.route("/which_button")
@@ -18,17 +18,49 @@ def which_button():
 @app.route("/button1")
 def button1():
   global whichButton
-  whichButton = "Button 1"
+  whichButton = "1"
   return whichButton
 
 @app.route("/button2")
 def button2():
   global whichButton
-  whichButton = "Button 2"
+  whichButton = "2"
+  return whichButton
+
+@app.route("/button3")
+def button3():
+  global whichButton
+  whichButton = "3"
+  return whichButton
+
+@app.route("/button4")
+def button4():
+  global whichButton
+  whichButton = "4"
+  return whichButton
+
+@app.route("/button5")
+def button5():
+  global whichButton
+  whichButton = "5"
+  return whichButton
+
+@app.route("/button6")
+def button6():
+  global whichButton
+  whichButton = "6"
+  return whichButton
+
+@app.route("/button7")
+def button7():
+  global whichButton
+  whichButton = "7"
   return whichButton
 
 if __name__ == "__main__":
   app.run("10.0.42.201", debug = True, threaded = True)
+  # app.run(debug = True, threaded = True)
+  # app.run("192.168.1.245", debug = True, threaded = True)
   whichButton = "hi"
 
 
